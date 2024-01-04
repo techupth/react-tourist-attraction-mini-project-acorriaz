de18055a81094aa1773a04d789859dd25f70b8b5
acorriaz <mikey.swx@gmail.com>
2024-01-04 12:59:30 +0700

    feat: add getTrips function to fetch all trips from database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [fetchData, setFetchData] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getTrips();
+  }, []);
+
+  const getTrips = async () => {
+    const result = await axios.get("http://localhost:4001/trips?keywords=");
+    setFetchData(() => result.data.data);
+  };
+
+  console.log(fetchData);
 
   return <div className="App">{/* Start coding here */}</div>;
 }
