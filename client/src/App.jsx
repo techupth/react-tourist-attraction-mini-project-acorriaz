64f26fc2a2ae5d27e5d706aa83a1b07a1ed9f414
acorriaz <mikey.swx@gmail.com>
2024-01-04 12:42:33 +0700

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+
+  useEffect(() => {}, []);
+
   return <div className="App">{/* Start coding here */}</div>;
 }
 
